b152af73c57e78ffabf712f31d91e8d7e693045e
XINICOM <XINICOM@outlook.com>
2026-08-07 00:05:38 +0800

    ADD: search opt

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,9 +4,19 @@
 
 vim.opt.loadplugins = true
 vim.opt.wrap = true
+
+-- 跳转到第一个匹配
+vim.opt.incsearch = true
+
+-- 搜索大小写
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- 使用自定义的终端
 vim.opt.shell = "cmd /c set PWSH_INIT_MODE=_nvim && pwsh -nologo"
 
-vim.opt.tabstop = 4 -- 一个 <Tab> 等于 4 个空格
-vim.opt.shiftwidth = 4 -- 缩进级别（按 >> 或 << 时移动的空格数）
-vim.opt.softtabstop = 4 -- 输入 <Tab> 时的空格数
-vim.opt.expandtab = true -- 用空格代替 tab 字符
+-- 缩进相关
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
