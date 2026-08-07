7482f8dc0200598a6ccadfb28f600667dbe588c1
XINICOM <XINICOM@outlook.com>
2026-08-07 17:58:13 +0800

    FIX: blink.cmp md buffer

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,19 +1,13 @@
 return {
 	{
 		"saghen/blink.cmp",
-		opts = function(_, opts)
-			--     opts.sources.default = function()
-			--         if vim.bo.filetype == "markdown" then
-			--             return { "lsp", "path", "snippets" }
-			--         else
-			--             return { "lsp", "path", "snippets", "buffer" }
-			--         end
-			--     end
-			if vim.bo.filetype == "markdown" then
-				opts.sources.default = { "lsp", "path", "snippets" }
-			else
-				opts.sources.default = { "lsp", "path", "snippets", "buffer" }
-			end
-		end,
+		opts = {
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				per_filetype = {
+					markdown = { "lsp", "path", "snippets" },
+				},
+			},
+		},
 	},
 }
